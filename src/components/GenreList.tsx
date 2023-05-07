147c96665220b68dc6dead4153d3c4065fba11e9
MSHO <msheikhoveisi@gmail.com>
2023-05-07 13:10:40 +0330

    Create a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,15 @@
 import { Text } from "@chakra-ui/react";
+import useData from "../hooks/useData";
 import useGenres, { Genre } from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres, error, isLoading } = useGenres();
+  const { data, error, isLoading } = useGenres();
   return (
     <>
       {error && <Text>error</Text>}
       {isLoading && <Text>Genres is loading</Text>}
       <ul>
-        {genres.map((genre) => (
+        {data.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
         ))}
       </ul>
