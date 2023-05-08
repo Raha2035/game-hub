82872d9138af5ed255e8fd7f59d9bdbdd3e0285e
MSHO <msheikhoveisi@gmail.com>
2023-05-08 12:36:27 +0330

    Fix the issue with Chakra menus

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -20,8 +20,6 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
     (order) => order.value === sortOrder
   );
 
-  console.log(currentSortOrder);
-
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
