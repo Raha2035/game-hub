afc6f9f2272d5647c4d42af222716bd2b8b35ea7
MSHO <msheikhoveisi@gmail.com>
2023-07-05 16:23:04 +0330

    Fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,24 @@
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text align={"justify"} marginY={3}>
+        {game.description_raw}
+      </Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
